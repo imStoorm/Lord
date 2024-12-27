dedb93b86e2a3d593213ad8d8055f3deb2defdb0
imStoorm <imstoormdev@proton.me>
2024-12-27 05:03:11 -0300

    fix: ensure it doesnt have any errors during build

--- app/_components/Bible.tsx
+++ app/_components/Bible.tsx
@@ -10,19 +10,29 @@ import Versicle from "./Versicle";
 
 const myFont = localFont({ src: "../fonts/Zodiak-Light.ttf" });
 
+interface Texto {
+  number: number;
+  text: string;
+}
+
 const Biblia = () => {
   const [livro, setLivro] = useState("Gênesis");
   const [capitulo, setCapitulo] = useState(1);
-  const [versiculo, setVersiculo] = useState(1);
-  const [texto, setTexto] = useState([]);
+  const [versiculo, setVersiculo] = useState(1); 
+  const [texto, setTexto] = useState<Texto[]>([]); 
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const loadText = async () => {
-        setLoading(true);
+      setLoading(true);
+      try {
         const response = await getText({ livro, capitulo });
         setTexto(response);
+      } catch (error) {
+        console.error("Erro ao carregar o texto:", error);
+      } finally {
         setLoading(false);
+      }
     };
 
     loadText();
@@ -32,12 +42,20 @@ const Biblia = () => {
     <div className="w-full h-[86%] flex items-center justify-center">
       <div className={`${myFont.className} text-center`}>
         <div className="flex gap-2 justify-center items-center mb-6">
-          <h1 className="text-white text-3xl">{livro} {capitulo}</h1>
+          <h1 className="text-white text-3xl">
+            {livro} {capitulo}
+          </h1>
         </div>
-        <div className=" w-[50%] h-[30%] mx-auto text-white p-4">
-          {loading ? "Carregando..." : texto?.map((item) => (
-            <Versicle key={item?.number} number={item?.number} text={item.text} />
-          ))}
+        <div className="w-[50%] h-[30%] mx-auto text-white p-4">
+          {loading ? (
+            "Carregando..."
+          ) : (
+            <ScrollArea className="overflow-y-auto">
+              {texto.map((item) => (
+                <Versicle key={item.number} number={item.number} text={item.text} />
+              ))}
+            </ScrollArea>
+          )}
         </div>
       </div>
     </div>
