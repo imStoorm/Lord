b8548b0170cf59e6a8f825a8d883169a75f0567d
imStoorm <imstoormdev@proton.me>
2024-12-28 12:16:51 -0300

    chore: user can't go to the next chapter if it doesnt exist

--- app/biblia/page.tsx
+++ app/biblia/page.tsx
@@ -18,7 +18,6 @@ import {
 } from "../_components/ui/select";
 import siglas from "../../public/siglas.json";
 
-
 interface LivroInfo {
   nome: string;
   capitulos: number;
@@ -26,7 +25,7 @@ interface LivroInfo {
 
 type SiglasType = Record<string, LivroInfo>;
 
-const siglasData = siglas as SiglasType; 
+const siglasData = siglas as SiglasType;
 
 const myFont = localFont({ src: "../fonts/Zodiak-Light.ttf" });
 
@@ -42,7 +41,6 @@ export default function Bible() {
   const [texto, setTexto] = useState<Texto[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
-
   const livros = Object.entries(siglasData);
 
   const getNumeroCapitulos = (): number => {
@@ -51,6 +49,8 @@ export default function Bible() {
     );
     return livroSigla ? siglasData[livroSigla].capitulos : 1;
   };
+
+  console.log(capitulo, getNumeroCapitulos(), capitulo >= getNumeroCapitulos());
 
   useEffect(() => {
     const loadText = async () => {
@@ -70,47 +70,54 @@ export default function Bible() {
     <div className="flex flex-col h-screen">
       <Header page="bible" className="sticky top-0 z-10" />
 
-      <div className={`w-[50%] h-[8%] mx-auto flex gap-3 items-center justify-between text-white p-4 border-b border-gray-300 relative`}>
+      <div
+        className={`w-[50%] h-[8%] mx-auto flex gap-3 items-center justify-between text-white p-4 border-b border-gray-300 relative`}
+      >
         <div className="flex-1" />
-        <h1 className={`${myFont.className} text-white text-3xl absolute left-1/2 transform -translate-x-1/2`}>
+        <h1
+          className={`${myFont.className} text-white text-3xl absolute left-1/2 transform -translate-x-1/2`}
+        >
           {livro} {capitulo}
         </h1>
-        <Select onValueChange={(value) => {
-          const nomeLivro = siglasData[value as keyof SiglasType].nome;
-          setLivro(nomeLivro);
-          setCapitulo(1); 
-        }}>
-        <SelectTrigger className="w-[100px]">
-          <SelectValue placeholder={livro} />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Livros</SelectLabel>
-            {livros.map(([sigla, dados]) => (
-              <SelectItem key={sigla} value={sigla}>
-                {dados.nome}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select onValueChange={(value) => setCapitulo(Number(value))}>
-        <SelectTrigger className="w-[70px]">
-          <SelectValue placeholder={capitulo}/>
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Capítulos</SelectLabel>
-            {Array.from({ length: getNumeroCapitulos() }, (_, i) => i + 1).map(
-              (num) => (
+        <Select
+          onValueChange={(value) => {
+            const nomeLivro = siglasData[value as keyof SiglasType].nome;
+            setLivro(nomeLivro);
+            setCapitulo(1);
+          }}
+        >
+          <SelectTrigger className="w-[100px]">
+            <SelectValue placeholder={livro} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Livros</SelectLabel>
+              {livros.map(([sigla, dados]) => (
+                <SelectItem key={sigla} value={sigla}>
+                  {dados.nome}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select onValueChange={(value) => setCapitulo(Number(value))}>
+          <SelectTrigger className="w-[70px]">
+            <SelectValue placeholder={capitulo} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Capítulos</SelectLabel>
+              {Array.from(
+                { length: getNumeroCapitulos() },
+                (_, i) => i + 1
+              ).map((num) => (
                 <SelectItem key={num} value={num.toString()}>
                   {num}
                 </SelectItem>
-              )
-            )}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
 
       <div className="flex-grow flex flex-col items-center justify-between overflow-y-auto">
@@ -127,18 +134,18 @@ export default function Bible() {
 
         <div className="flex justify-center my-10 gap-10">
           <Button
-            className={`${inter.className} items-center cursor-pointer text-white hover:text-gray-400`}
+            className={`${inter.className} items-center cursor-pointer text-white hover:text-gray-400 disabled:opacity-50`}
             variant="outline"
-            asChild
             onClick={() => setCapitulo((prev) => Math.max(prev - 1, 1))}
+            disabled={capitulo <= 1}
           >
             <h1>Capítulo Anterior</h1>
           </Button>
           <Button
-            className={`${inter.className} items-center cursor-pointer text-white hover:text-gray-400`}
+            className={`${inter.className} items-center cursor-pointer text-white hover:text-gray-400 disabled:opacity-50`}
             variant="outline"
-            asChild
-            onClick={() => setCapitulo((prev) => prev + 1)}
+            disabled={capitulo >= getNumeroCapitulos()}
+            onClick={() => {capitulo >= getNumeroCapitulos() ? null : setCapitulo((prev) => prev + 1)}}
           >
             <h1>Próximo Capítulo</h1>
           </Button>
